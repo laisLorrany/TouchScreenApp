08a52c12a97b1e2f669038aac8e2e21bf6b7596f
Laís Lorrany <lorranyand@gmail.com>
2016-03-19 11:03:11 -0300

    Exibição da inclinação utilizando o sensor acelerômetro.

--- TouchApp/src/br/edu/ifpb/touchapp/activity/MainActivity.java
+++ TouchApp/src/br/edu/ifpb/touchapp/activity/MainActivity.java
@@ -1,6 +1,10 @@
 package br.edu.ifpb.touchapp.activity;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MotionEvent;
@@ -9,11 +13,42 @@ import android.view.View.OnTouchListener;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	/*
+	 * Para o acelerômetro:
+	 * http://ubuntu.blog.br/acelerometro-e-sensor-de-gravidade-no-android/
+	 * */
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		
+		SensorEventListener sensorEvent = new SensorEventListener() {
+			
+			@Override
+			public void onSensorChanged(SensorEvent event) {
+				
+				if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+					float inclinacao = event.values[0];
+					
+					TextView movimento = (TextView) findViewById(R.id.movimentoTextView);
+					movimento.setText("Inclinação: " + inclinacao);
+				}
+				
+			}
+			
+			@Override
+			public void onAccuracyChanged(Sensor sensor, int accuracy) {
+								
+			}
+		};
+		((SensorManager)getSystemService(SENSOR_SERVICE)).
+		registerListener(sensorEvent,((SensorManager)getSystemService(SENSOR_SERVICE)).
+				getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.
+				SENSOR_DELAY_GAME);
+		   
 	}
 
 	@Override
@@ -21,7 +56,7 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 
-		final TextView coordenadasTextView = (TextView) findViewById(R.id.coordenadasTextView);
+		final TextView coordenadasTextView = (TextView) findViewById(R.id.coordenadasXYTextView);
 		coordenadasTextView.setOnTouchListener(new OnTouchListener() {
 			@Override
 			public boolean onTouch(View view, MotionEvent event) {
